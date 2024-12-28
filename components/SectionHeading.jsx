a3ad38924793d2068c7aca60afa3a91ddfd58876
Shalvin <shalvinshaji283@gmail.com>
2024-12-28 22:58:47 +0530

    section heading component created

---
+++ components/SectionHeading.jsx
@@ -0,0 +1,16 @@
+const SectionHeading = () => {
+  return (
+    <div className="text-start mb-12">
+      <h2 className="text-4xl font-extrabold text-mahogany mb-4 tracking-wide">
+        Our Services
+      </h2>
+      <p className="text-lg font-semibold text-gray-600 ">
+        Offering top-notch solutions tailored to your unique needs. Explore our
+        services and let us help you achieve your goals.
+      </p>
+      <div className="mt-6 border-t-4 border-mahogany w-16 mx-auto"></div>
+    </div>
+  );
+};
+
+export default SectionHeading;
